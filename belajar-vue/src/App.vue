39de3047b5a14f73a0b70c84c10a215572dcc1cf
DigdayaRajab <rdigdaya84@gmail.com>
2022-07-26 20:51:46 +0700

    try vue app

--- belajar-vue/src/App.vue
+++ belajar-vue/src/App.vue
@@ -1,20 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<!-- <script setup> -->
+// import HelloWorld from "./components/HelloWorld.vue"; // import TheWelcome from "./components/TheWelcome.vue";
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+    </div> -->
+  <!-- </header>
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
+
+  <div id="app">
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
 </template>
 
 <style scoped>
--- belajar-vue/src/main.js
+++ belajar-vue/src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import './assets/main.css'
+// import "./assets/main.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+// const app = createApp({
+//   data() {
+//     return {
+//       count: 0,
+//     };
+//   },
+// });
+
+// app.mount("#app");
